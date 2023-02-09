50d12d155c75da325f928806fa58c452ca4750e4
alaaeddin2p <a.istanbuli@tawasulforum.org>
2023-02-09 17:59:45 +0300

    Almost finished header (need final test)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,100 @@
-/* ===[Start Language Swicher]=== */
 window.addEventListener('DOMContentLoaded', function () {
+    /* ===[Start Language Swicher]=== */
     let langSwichBtns = document.querySelectorAll('.lang-switcher button');
-    
+
     langSwichBtns.forEach((langSwichBtn) => {
-        langSwichBtn.addEventListener('click', function(){
+        langSwichBtn.addEventListener('click', function () {
             this.parentElement.classList.toggle('active');
-        });  
+        });
     });
 
-    window.addEventListener('click', function(e){
+    window.addEventListener('click', function (e) {
         // Close Language Swicher
-        if(!e.target.parentElement.classList.contains('lang-switcher')) {
-            let switcherDropdowns = document.querySelectorAll('.lang-switcher--dropdown');
-            
-            switcherDropdowns.forEach((switcherDropdown) => {
-                switcherDropdown.parentElement.classList.remove('active');
-            });
+        if (e.target.tagName !== 'HTML') {
+            if (!e.target.parentElement.classList.contains('lang-switcher')) {
+                let switcherDropdowns = document.querySelectorAll('.lang-switcher--dropdown');
+
+                switcherDropdowns.forEach((switcherDropdown) => {
+                    switcherDropdown.parentElement.classList.remove('active');
+                });
+            }
         }
     });
+    /* ===[End Language Swicher]=== */
+
+    /* ===[Start Mobile Header]=== */
+    if (window.innerWidth <= 1024) {
+        /* ===[Start Mobile Header]=== */
+        let mainHeaderTag = document.querySelector('header');
+        let toggleMobileMenuBtn = document.querySelector('.burger-btn');
+        let mobileMenuNav = document.querySelector('.bottom-header');
+
+        // Toggle Button Handler
+        toggleMobileMenuBtn.addEventListener('click', function () {
+            this.classList.toggle('active');
+            mobileMenuNav.classList.toggle('active-nav');
+            mainHeaderTag.classList.toggle('expanded-nav');
+        });
+
+        document.querySelectorAll('.sub-menu .sub-menu').forEach((subMenu) => {
+            subMenu.classList.add('next-level');
+        });
+
+        mobileMenuNav.addEventListener('click', function (e) {
+            e.stopPropagation();
+
+            if (e.target.classList.contains('menu-arrow')) {
+
+                // In case of clicking outside but not the nested level of sub menu
+                if (!e.target.nextElementSibling.classList.contains('next-level')) {
+                    document.querySelectorAll('.sub-menu').forEach((subMenu) => {
+                        subMenu.style.display = 'none';
+                    });
+
+                    document.querySelectorAll('.next-level').forEach((nextLevel) => {
+                        nextLevel.classList.remove('opened');
+                        e.target.style.transform = 'rotateX(0deg)';
+                    })
+                }
+
+                // In case of opening already opened sub menu (It will close)
+                if (e.target.nextElementSibling.classList.contains('opened')) {
+                    e.target.nextElementSibling.style.display = 'none';
+                    e.target.nextElementSibling.classList.remove('opened');
+                    e.target.style.transform = 'rotateX(0deg)';
+                }
+
+                // In case of click the arrow to open a closed sub menu (It will open)
+                else {
+                    if (document.querySelectorAll('.sub-menu.opened').length > 0 && !e.target.nextElementSibling.classList.contains('next-level')) {
+                        document.querySelectorAll('.sub-menu.opened').forEach((subMenuOpened) => {
+                            subMenuOpened.classList.remove('opened');
+                        })
+                    }
+
+                    if (!e.target.nextElementSibling.classList.contains('next-level')) {
+                        document.querySelectorAll('.menu-arrow').forEach((menuArrow) => menuArrow.style.transform = 'rotateY(0deg)');
+                    }
+
+                    e.target.nextElementSibling.style.display = 'block';
+                    e.target.nextElementSibling.classList.add('opened');
+                    e.target.style.transform = 'rotateX(180deg)';
+                }
+            }
+
+            // In case if the click isn't on the arrow
+            else {
+                // if the clicked element isn't containing menu-item class which is the <li> then close any opened sub menus
+                if (!e.target.parentElement.classList.contains('sub-menu')) {
+                    document.querySelectorAll('.menu-arrow').forEach((menuArrow) => menuArrow.style.transform = 'rotateY(0deg)');
+
+                    document.querySelectorAll('.sub-menu').forEach((subMenu) => {
+                        subMenu.style.display = 'none';
+                        subMenu.classList.remove('opened');
+                    });
+                }
+            }
+        });
+    }
+    /* ===[End Mobile Header]=== */
 });
-/* ===[End Language Swicher]=== */
--- header.php
+++ header.php
@@ -37,6 +37,7 @@
 							</a>
 						</div>
 
+						<!-- Donate button -->
 						<div class="donation-button-holder from-laptop-up as-block mx-3">
 							<a href="#" class="primary-btn donation-btn">
 								<span>Donate Now</span>
@@ -62,7 +63,7 @@
 						</div>
 
 						<!-- Login Button -->
-						<div class="me-lg-3 login">
+						<div class="ms-3 ms-lg-0 me-2 me-lg-3 login">
 							<button class="secondary-outlined-btn login-btn">
 								<svg id="Group_442" data-name="Group 442" xmlns="http://www.w3.org/2000/svg" width="26" height="26" viewBox="0 0 26 26">
 									<path id="Path_304" data-name="Path 304" d="M0,0H26V26H0Z" fill="none" />
@@ -81,7 +82,7 @@
 								</div>
 							</div>
 						</div>
-						
+
 						<!-- Language Swicher -->
 						<div class="lang-switcher from-laptop-up as-block">
 							<button class="secondary-outlined-btn">
@@ -124,6 +125,59 @@
 							);
 							?>
 						</nav>
+
+						<div class="mobile-nav-items from-ipad-down as-block mt-auto">
+							<!-- Search -->
+							<div class="header-search from-ipad-down as-block">
+								<form method="get" action="<?php echo home_url('/'); ?>" class="search-form">
+									<div class="input-holder search-input-holder">
+										<div class="search-icon">
+											<svg id="Group_262" data-name="Group 262" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+												<path id="Path_153" data-name="Path 153" d="M0,0H24V24H0Z" fill="none" />
+												<path id="Path_154" data-name="Path 154" d="M18.031,16.617,22.314,20.9,20.9,22.314l-4.282-4.283a9,9,0,1,1,1.414-1.414Zm-2.006-.742a7,7,0,1,0-.15.15l.15-.15Z" fill="#6d54a7" />
+											</svg>
+										</div>
+										<input type="search" name="s" id="header-search-input" placeholder="<?php _e('Search...', 'bonyan') ?>" class="ps-5 pe-2">
+									</div>
+								</form>
+							</div>
+
+							<div class="mobile-header-cta from-ipad-down as-flex align-items-center">
+								<!-- Donate button -->
+								<div class="donation-button-holder me-3 my-2">
+									<a href="#" class="primary-btn donation-btn">
+										<span>Donate Now</span>
+										<svg xmlns="http://www.w3.org/2000/svg" width="20" height="18.485" viewBox="0 0 20 18.485">
+											<path id="Path_150" data-name="Path 150" d="M12,4.529a6,6,0,0,1,8.478,8.464L12,21.485,3.521,12.993A6,6,0,0,1,12,4.529Z" transform="translate(-2 -3)" fill="#fff" />
+										</svg>
+									</a>
+								</div>
+
+								<!-- Language Swicher -->
+								<div class="lang-switcher">
+									<button class="secondary-outlined-btn">
+										<span class="me-2 current-lang">En</span>
+
+										<svg class="me-1" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20">
+											<path id="Path_213" data-name="Path 213" d="M12,22A10,10,0,1,1,22,12,10,10,0,0,1,12,22ZM9.71,19.667A17.9,17.9,0,0,1,8.027,13H4.062A8.008,8.008,0,0,0,9.71,19.667ZM10.03,13A15.915,15.915,0,0,0,12,19.752,15.9,15.9,0,0,0,13.97,13H10.03Zm9.908,0H15.973a17.9,17.9,0,0,1-1.683,6.667A8.008,8.008,0,0,0,19.938,13ZM4.062,11H8.027A17.9,17.9,0,0,1,9.71,4.333,8.008,8.008,0,0,0,4.062,11Zm5.969,0h3.938A15.9,15.9,0,0,0,12,4.248,15.9,15.9,0,0,0,10.03,11ZM14.29,4.333A17.9,17.9,0,0,1,15.973,11h3.965A8.008,8.008,0,0,0,14.29,4.333Z" transform="translate(-2 -2)" fill="#5b4795" />
+										</svg>
+
+										<svg xmlns="http://www.w3.org/2000/svg" width="8.486" height="5.657" viewBox="0 0 8.486 5.657">
+											<path id="Path_215" data-name="Path 215" d="M12,15,7.757,10.757,9.172,9.343,12,12.172l2.828-2.829,1.415,1.414Z" transform="translate(-7.757 -9.343)" fill="#5b4795" />
+										</svg>
+									</button>
+
+									<ul class="lang-switcher--dropdown">
+										<div class="menu-languages-container">
+											<ul id="language-menu" class="menu">
+												<li id="menu-item-wpml-ls-43-en" class="menu-item wpml-ls-slot-43 wpml-ls-item wpml-ls-item-en wpml-ls-current-language wpml-ls-menu-item wpml-ls-last-item menu-item-type-wpml_ls_menu_item menu-item-object-wpml_ls_menu_item menu-item-wpml-ls-43-en"><a title="English" href="https://bonyan-sy.org/beta/"><span class="wpml-ls-native" lang="en">Arabic</span></a><i class="menu-arrow"></i></li>
+											</ul>
+										</div>
+									</ul>
+								</div>
+							</div>
+						</div>
+
 					</div>
 				</div>
 
