a59fc5388510ad803e7a67ab84c01fd581c13b37
alaaeddin2p <a.istanbuli@tawasulforum.org>
2023-02-09 11:08:12 +0300

    Changed menu location and id

--- functions.php
+++ functions.php
@@ -51,7 +51,7 @@ function bonyan_setup() {
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus(
 		array(
-			'menu-1' => esc_html__( 'Primary', 'bonyan' ),
+			'main_menu' => esc_html__( 'Primary', 'bonyan' ),
 		)
 	);
 
