72cb97cee246be263db3240946aca539ade9764c
alaaeddin2p <a.istanbuli@tawasulforum.org>
2023-02-27 17:37:31 +0300

    Single Vacancy (Front-end)

---
+++ page-sample-single-vacancy.php
@@ -0,0 +1,37 @@
+<?php get_header(); ?>
+<?php echo get_template_part('template-parts/page-head'); ?>
+
+<div class="container">
+    <div class="content-with-info-panel">
+        <div class="inner-content">
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet, soluta. Ipsum illo nisi, enim sapiente animi officia dicta a culpa omnis hic suscipit neque beatae molestias molestiae ullam! Praesentium nam rerum asperiores saepe dolorum a accusantium nobis aliquam voluptatem, officiis quaerat earum eaque hic esse repudiandae aliquid repellendus sint quia dicta culpa eum sed obcaecati dolor. Quod quos enim earum! Molestiae vero consequatur illo animi reiciendis atque eum fugiat distinctio error. Nulla dolorem id soluta at exercitationem iste aspernatur neque, commodi quas sunt inventore, alias tenetur cumque odit illo enim obcaecati laboriosam sint possimus ullam consequatur porro! Illo, voluptate temporibus.</p>
+        </div>
+
+        <div class="info-panel">
+            <div class="info-panel--card with-light">
+                <h2 class="info-panel-title">Job Details</h2>
+
+                <div class="info-box">
+                    <div class="info-item">
+                        <span>Deadline: 30 Dec 2022</span>
+                    </div>
+
+                    <div class="info-item">
+                        <span>Job Type: on site</span>
+                    </div>
+
+                    <div class="info-item">
+                        <span>Location: Istanbul, Turkey</span>
+                    </div>
+                </div>
+
+                <div class="info-cta">
+                    <a href="#" class="primary-btn">Apply for this job</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+
+<?php get_footer();
