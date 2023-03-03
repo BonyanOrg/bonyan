108b1520b4f38cb24499a1ec8cd7414b0068ebd7
Abd-Basaleh <a.basaleh@tawasulforum.org>
2023-03-03 13:11:57 +0300

    Support Child Category View

--- template-parts/search-terms-header.php
+++ template-parts/search-terms-header.php
@@ -5,6 +5,7 @@
                 $terms = get_terms(array(
                     'taxonomy' => $args['taxonomy_name'],
                     'hide_empty' => false,
+                    'parent' => 0
                 ));
                 if (count($terms) > 0) {
                     foreach ($terms as $term) {
@@ -25,28 +26,49 @@
 <?php elseif (!isset($args['archive'])) : ?>
     <div class="categories-and-search">
         <div class="categories-filter-scroll">
-        <div class="categories-filter">
-            <?php if (!empty($args['taxonomy_name'])) {
-                $terms = get_terms(array(
-                    'taxonomy' => $args['taxonomy_name'],
-                    'hide_empty' => true,
-                ));
+            <div class="categories-filter">
+                <?php if (!empty($args['taxonomy_name'])) {
+                    $parent_id = get_term($args['queried_object']->term_id, $args['taxonomy_name'])->parent;
 
-                if (count($terms) > 0) {
-                    foreach ($terms as $term) {
-                        $active = "";
-                        if ($term->term_id == $args['queried_object']->term_id) {
-                            $active = "active";
+                    $terms = get_terms(array(
+                        'taxonomy' => $args['taxonomy_name'],
+                        'hide_empty' => true,
+                        'parent' => 0
+                    ));
+                    $term_childrens = get_terms(array(
+                        'taxonomy' => $args['taxonomy_name'],
+                        'hide_empty' => true,
+                        'hierarchical'          => 1,
+                        'child_of'              => !empty($parent_id) ? $parent_id : $args['queried_object']->term_id,
+
+                    ));
+                    if (count($term_childrens) > 0 || !empty($parent_id)) {
+                        foreach ($term_childrens as $term) {
+                            $active = "";
+                            if ($term->term_id == $args['queried_object']->term_id) {
+                                $active = "active";
+                            }
+                        ?>
+                            <a href=" <?php echo get_term_link($term->term_id) ?> " class="category-filter-item <?php echo $active ?>">
+                                <span><?php echo $term->name  ?></span>
+                            </a>
+                        <?php
                         }
-            ?>
-                        <a href=" <?php echo get_term_link($term->term_id) ?> " class="category-filter-item <?php echo $active ?>">
-                            <span><?php echo $term->name  ?></span>
-                        </a>
-            <?php
+                    } else if (count($terms) > 0) {
+                        foreach ($terms as $term) {
+                            $active = "";
+                            if ($term->term_id == $args['queried_object']->term_id) {
+                                $active = "active";
+                            }
+                        ?>
+                            <a href=" <?php echo get_term_link($term->term_id) ?> " class="category-filter-item <?php echo $active ?>">
+                                <span><?php echo $term->name  ?></span>
+                            </a>
+                <?php
+                        }
                     }
-                }
-            } ?>
-        </div>
+                } ?>
+            </div>
         </div>
 
 
