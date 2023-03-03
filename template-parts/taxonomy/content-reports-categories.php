db47ceb566f36caed776077c8df7503a28974088
Abd-Basaleh <a.basaleh@tawasulforum.org>
2023-03-03 13:12:25 +0300

    Show Last Report In Report Cat Page

--- template-parts/taxonomy/content-reports-categories.php
+++ template-parts/taxonomy/content-reports-categories.php
@@ -10,6 +10,35 @@ $taxonomy_name = $queried_object->taxonomy;
 <section class="campaign-section">
     <div class="container">
 
+        <div class="content-with-info-panel">
+            <!-- <div class="inner-content">
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet, soluta. Ipsum illo nisi, enim sapiente animi officia dicta a culpa omnis hic suscipit neque beatae molestias molestiae ullam! Praesentium nam rerum asperiores saepe dolorum a accusantium nobis aliquam voluptatem, officiis quaerat earum eaque hic esse repudiandae aliquid repellendus sint quia dicta culpa eum sed obcaecati dolor. Quod quos enim earum! Molestiae vero consequatur illo animi reiciendis atque eum fugiat distinctio error. Nulla dolorem id soluta at exercitationem iste aspernatur neque, commodi quas sunt inventore, alias tenetur cumque odit illo enim obcaecati laboriosam sint possimus ullam consequatur porro! Illo, voluptate temporibus.</p>
+            </div> -->
+
+            <div class="info-panel">
+                <div class="file-card">
+                    <!-- File Icon (No need to be dynamic) -->
+                    <div class="file-icon">
+                        <img data-src="<?php echo get_template_directory_uri() . '/dist/imgs/report-icon.png'; ?>" alt="" class="lazyload">
+                    </div>
+
+                    <!-- File Name -->
+                    <h3 class="file-name">Last Report</h3>
+
+                    <!-- File CTAs -->
+                    <div class="file-cta">
+                        <?php
+                        $last_post_id = $posts[0]->ID;
+                        $PDF_file = get_post_meta($last_post_id, 'ro_reports_pdf_file', true);
+                        $PDF_file_url = wp_get_attachment_url($PDF_file);
+                        ?>
+                        <a href="<?php echo $PDF_file_url ?>" class="reversed-primary-btn preview-file">Preview</a>
+                        <a href="<?php echo $PDF_file_url ?>" class="primary-btn download-file">Download the file</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
         <?php get_template_part('template-parts/search-terms-header', null, array("taxonomy_name" => $taxonomy_name, "queried_object" => $queried_object)); ?>
 
 
@@ -20,15 +49,19 @@ $taxonomy_name = $queried_object->taxonomy;
                 <?php if (have_posts()) : ?>
 
                     <?php
+                    $counter = 0;
                     /* Start the Loop */
                     while (have_posts()) :
                         the_post();
+                        // if ($counter == 0) { // Skip First Post
+                        //     $counter++;
+                        //     continue;
+                        // }
 
                     ?>
                         <?php get_template_part('template-parts/cards/content', $post->post_type); ?>
 
                 <?php
-
 
                     endwhile;
 
