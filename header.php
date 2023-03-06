2663cfbbf788b507c3994e9eb6599a59f899cc40
alaaeddin2p <a.istanbuli@tawasulforum.org>
2023-03-06 17:35:42 +0300

    Handled the scenario when the user logged in by adding its avatar to it (Fornt-end)

--- header.php
+++ header.php
@@ -85,7 +85,7 @@
 									</svg>
 								</button>
 							<?php else : ?>
-								<a href="<?php echo home_url('/dashboard'); ?>" class="secondary-outlined-btn">
+								<a href="<?php echo home_url('/dashboard'); ?>" class="secondary-outlined-btn logged-user-avatar">
 									<img data-src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="User Avatar" class="lazyload">
 								</a>
 							<?php endif; ?>
