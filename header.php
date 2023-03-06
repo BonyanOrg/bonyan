1f3886654702e6c1803007b1396111502f40af15
Abd-Basaleh <a.basaleh@tawasulforum.org>
2023-03-06 16:51:50 +0300

    add switcher on user logged in or not

--- header.php
+++ header.php
@@ -77,12 +77,18 @@
 
 						<!-- Login Button -->
 						<div class="ms-3 ms-lg-0 me-2 me-lg-3 login">
-							<button class="secondary-outlined-btn user-action-btn" data-target="login-modal">
-								<svg id="Group_442" data-name="Group 442" xmlns="http://www.w3.org/2000/svg" width="26" height="26" viewBox="0 0 26 26">
-									<path id="Path_304" data-name="Path 304" d="M0,0H26V26H0Z" fill="none" />
-									<path id="Path_305" data-name="Path 305" d="M14.833,15.356V17.62a6.5,6.5,0,0,0-8.667,6.13H4a8.667,8.667,0,0,1,10.833-8.394ZM12.667,14a6.5,6.5,0,1,1,6.5-6.5A6.5,6.5,0,0,1,12.667,14Zm0-2.167A4.333,4.333,0,1,0,8.333,7.5,4.332,4.332,0,0,0,12.667,11.833Zm7.135,6.5-1.981-1.98,1.533-1.533,4.6,4.6-4.6,4.6L17.82,22.48,19.8,20.5H15.917V18.333Z" transform="translate(0.333 0.083)" fill="#5f469a" />
-								</svg>
-							</button>
+							<?php if (!is_user_logged_in()) :  ?>
+								<button class="secondary-outlined-btn user-action-btn" data-target="login-modal">
+									<svg id="Group_442" data-name="Group 442" xmlns="http://www.w3.org/2000/svg" width="26" height="26" viewBox="0 0 26 26">
+										<path id="Path_304" data-name="Path 304" d="M0,0H26V26H0Z" fill="none" />
+										<path id="Path_305" data-name="Path 305" d="M14.833,15.356V17.62a6.5,6.5,0,0,0-8.667,6.13H4a8.667,8.667,0,0,1,10.833-8.394ZM12.667,14a6.5,6.5,0,1,1,6.5-6.5A6.5,6.5,0,0,1,12.667,14Zm0-2.167A4.333,4.333,0,1,0,8.333,7.5,4.332,4.332,0,0,0,12.667,11.833Zm7.135,6.5-1.981-1.98,1.533-1.533,4.6,4.6-4.6,4.6L17.82,22.48,19.8,20.5H15.917V18.333Z" transform="translate(0.333 0.083)" fill="#5f469a" />
+									</svg>
+								</button>
+							<?php else : ?>
+								<a href="<?php echo home_url('/dashboard'); ?>" class="secondary-outlined-btn">
+									<img data-src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="User Avatar" class="lazyload">
+								</a>
+							<?php endif; ?>
 						</div>
 
 						<!-- Burger Button For Tablate/iPad & Mobile -->
