01ac670ec595c521ad1411a54846215d121e9d0e
Abd-Basaleh <a.basaleh@tawasulforum.org>
2023-03-09 10:24:25 +0300

    add search page add campaign give wp form in single campaign page if is set

--- search.php
+++ search.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying search results pages
  *
@@ -10,44 +11,60 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
 
-		<?php if ( have_posts() ) : ?>
+<div class="page-head" style="background: url(' <?php
+												?>') ">
+	<div class="container">
+		<h1>
+			<?php _e('Search', 'bonyan') ?>
+		</h1>
+		<nav aria-label="breadcrumbs" class="rank-math-breadcrumb">
+			<?php if (function_exists('rank_math_the_breadcrumbs')) rank_math_the_breadcrumbs(); ?>
+		</nav>
+	</div>
+</div>
 
-			<header class="page-header">
-				<h1 class="page-title">
-					<?php
-					/* translators: %s: search query. */
-					printf( esc_html__( 'Search Results for: %s', 'bonyan' ), '<span>' . get_search_query() . '</span>' );
-					?>
-				</h1>
-			</header><!-- .page-header -->
+
+
+<section class="blogs-section">
+	<div class="container">
+
+		<div class="categories-and-search">
+			<div class="input-holder search ms-auto">
+				<?php get_search_form() ?>
+			</div>
+		</div>
+
+		<div class="cards-container">
+
+			<?php if (have_posts()) : ?>
+
+				<?php
+				/* Start the Loop */
+				while (have_posts()) :
+					the_post();
+
+				?>
+						<?php get_template_part('template-parts/cards/content-post'); ?>
 
 			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
 
-				/**
-				 * Run the loop for the search to output the results.
-				 * If you want to overload this in a child theme then include a file
-				 * called content-search.php and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', 'search' );
 
-			endwhile;
+				endwhile;
 
-			the_posts_navigation();
+			//the_posts_navigation();
 
-		else :
+			else :
 
-			get_template_part( 'template-parts/content', 'none' );
+				get_template_part('template-parts/content', 'none');
 
-		endif;
+			endif;
+			?>
+
+		</div>
+		<?php custom_pagination();
 		?>
-
-	</main><!-- #main -->
+</section>
 
 <?php
-get_sidebar();
 get_footer();
--- single.php
+++ single.php
@@ -11,9 +11,16 @@
 get_header();
 ?>
 <?php get_template_part('template-parts/page', 'header'); ?>
-<div class="single-<?php echo get_post_type() ?>"  >
+<div class="single-<?php echo get_post_type() ?>">
 	<div class="container">
 		<div class="inner-content">
+		<?php
+		if ($post_type === "campaign") {
+			$give_form_id = get_post_meta(get_the_ID(), "co_give_form_id", true);
+			if (!empty($give_form_id)) {
+				echo do_shortcode('[give_form id="' . $give_form_id . '"]', true);
+			}
+		} ?>
 			<?php the_content(); ?>
 		</div>
 	</div>
