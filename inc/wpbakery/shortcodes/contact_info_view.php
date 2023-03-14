78cba3b76de76ee5710bac8c6e6e7e41a191dc30
Abd-Basaleh <a.basaleh@tawasulforum.org>
2023-03-14 13:01:42 +0300

    fix content info link on mail and phone

--- inc/wpbakery/shortcodes/contact_info_view.php
+++ inc/wpbakery/shortcodes/contact_info_view.php
@@ -12,6 +12,10 @@ if (!function_exists('contact_info_shortcode')) {
     {
         extract(shortcode_atts(array(
             'contact_info_icon' => '',
+            'contact_info_is_phone' => '',
+            'contact_info_is_email' => '',
+            'contact_info_phone_number' => '',
+            'contact_info_email' => '',
         ), $atts));
 
 
@@ -33,17 +37,23 @@ if (!function_exists('contact_info_shortcode')) {
 
         <!-- This is the START of the component -->
         <div class="contact-item">
-            <a href="javascript:void(0)" class="phone-numbers-container">
-                <div class="contact-icon">
-                    <?php if (!empty($contact_info_icon)) : ?>
-                        <img data-src="<?php echo wp_get_attachment_image_url($contact_info_icon); ?>" alt="<?php echo $content; ?>" class="lazyload">
-                    <?php endif; ?>
-                </div>
+            <?php if ($contact_info_is_phone) : ?>
+                <a href="tel:<?php echo $contact_info_phone_number; ?>" class="phone-numbers-container">
+                <?php elseif ($contact_info_is_email) : ?>
+                    <a href="mailto:<?php echo $contact_info_email; ?>" class="phone-numbers-container">
+                    <?php else : ?>
+                        <a href="#" class="phone-numbers-container">
+                        <?php endif; ?>
+                        <div class="contact-icon">
+                            <?php if (!empty($contact_info_icon)) : ?>
+                                <img data-src="<?php echo wp_get_attachment_image_url($contact_info_icon); ?>" alt="<?php echo $content; ?>" class="lazyload">
+                            <?php endif; ?>
+                        </div>
 
-                <div class="phone-numbers">
-                    <span><?php echo $content; ?></span>
-                </div>
-            </a>
+                        <div class="phone-numbers">
+                            <span><?php echo $content; ?></span>
+                        </div>
+                        </a>
         </div>
         <!-- This is the END of the component -->
 
--- inc/wpbakery/vcmaps/contact_info_map.php
+++ inc/wpbakery/vcmaps/contact_info_map.php
@@ -19,10 +19,50 @@ function contact_info_vc()
 			array(
 				"type"			=> "textarea_html",
 				"admin_label"	=> true,
-				"heading"		=> esc_html__(" Text", 'text_DOMAIN'),
+				"heading"		=> esc_html__("Text", 'text_DOMAIN'),
 				"param_name"	=> "content",
 				"value"			=> "",
 				"description"	=> esc_html__("Type a description to show under the section title.", 'text_DOMAIN'),
+			),
+			array(
+				"type"			=> "checkbox",
+				"admin_label"	=> false,
+				"heading"		=> esc_html__("Is Phone", 'text_DOMAIN'),
+				"param_name"	=> "contact_info_is_phone",
+				"value"			=> "",
+				"description"	=> esc_html__("Type a description to show under the section title.", 'text_DOMAIN'),
+			),
+			array(
+				"type"			=> "textfield",
+				"admin_label"	=> false,
+				"heading"		=> esc_html__("Phone Number", 'text_DOMAIN'),
+				"param_name"	=> "contact_info_phone_number",
+				"value"			=> "",
+				"description"	=> esc_html__("Type the phone number with out spaces please !", 'text_DOMAIN'),
+				'dependency'    => array(
+					'element'   => 'contact_info_is_phone',
+					'value' 	=> "true",
+				),
+			),
+			array(
+				"type"			=> "checkbox",
+				"admin_label"	=> false,
+				"heading"		=> esc_html__("Is Email", 'text_DOMAIN'),
+				"param_name"	=> "contact_info_is_email",
+				"value"			=> "",
+				"description"	=> esc_html__("Type a description to show under the section title.", 'text_DOMAIN'),
+			),
+			array(
+				"type"			=> "textfield",
+				"admin_label"	=> false,
+				"heading"		=> esc_html__("Email", 'text_DOMAIN'),
+				"param_name"	=> "contact_info_email",
+				"value"			=> "",
+				"description"	=> esc_html__("Type the email with out spaces please !", 'text_DOMAIN'),
+				'dependency'    => array(
+					'element'   => 'contact_info_is_email',
+					'value' 	=> "true",
+				),
 			),
 		)
 	));
