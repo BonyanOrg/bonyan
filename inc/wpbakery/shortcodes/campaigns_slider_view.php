b33950cc375cf32dfc372385259817d16498357a
mrawhani <mrawhani5@gmail.com>
2025-08-07 17:05:27 +0300

    enhance campigns slider

--- inc/wpbakery/shortcodes/campaigns_slider_view.php
+++ inc/wpbakery/shortcodes/campaigns_slider_view.php
@@ -46,55 +46,54 @@ if (!function_exists('campaigns_slider_shortcode')) {
 
         <!-- Start Campaign Cards -->
         <section class="campaigns-section py-5 custom-widget">
+                <div class="d-flex align-items-center justify-content-center justify-content-xl-strtch mb-3 ">
+                    <h2 class="bonyan-title primary-color bold"><?php echo $campaigns_slider_header_text ?></h2>
 
-            <div class="d-flex align-items-center justify-content-center justify-content-xl-strtch mb-3 ">
-                <h2 class="bonyan-title primary-color bold"><?php echo $campaigns_slider_header_text ?></h2>
+                    <div class="custom-swiper-nav ms-auto">
+                        <div class="swiper-nav-btn swiper-prev-nav campaigns-prev-arrow"></div>
+                        <div class="swiper-nav-btn swiper-next-nav campaigns-next-arrow"></div>
+                    </div>
 
-                <div class="custom-swiper-nav ms-auto">
-                    <div class="swiper-nav-btn swiper-prev-nav campaigns-prev-arrow"></div>
-                    <div class="swiper-nav-btn swiper-next-nav campaigns-next-arrow"></div>
+                    <a href="<?php echo $a_href ?>" class="more-btn secondary-outlined-btn ms-3 primary-color hide-from-laptop-up"><?php _e('see more', 'bonyan') ?></a>
                 </div>
 
-                <a href="<?php echo $a_href ?>" class="more-btn secondary-outlined-btn ms-3 primary-color hide-from-laptop-up"><?php _e('see more', 'bonyan') ?></a>
-            </div>
-
-
-            <div class="swiper campaigns-carousel">
-                <div class="swiper-wrapper">
-                    <?php
-                    $taxonomy = $post_tag != "" ? "campaigns-tags" : "campaigns-categories";
-                    $args = array(
-                        'post_type' => 'campaign',
-                        'post_status' => 'publish',
-                        'posts_per_page' => $campaigns_slider_cards_count,
-                        "tax_query" => array(
-                            array(
-                                "taxonomy" => $taxonomy,
-                                'field'    => 'slug',
-                                'terms'    => $post_tag != "" ? $post_tag : $campaigns_categories,
+                <div class="swiper campaigns-carousel">
+                    <div class="swiper-wrapper">
+                        <?php
+                        $taxonomy = $post_tag != "" ? "campaigns-tags" : "campaigns-categories";
+                        $args = array(
+                            'post_type' => 'campaign',
+                            'post_status' => 'publish',
+                            'posts_per_page' => $campaigns_slider_cards_count,
+                            "tax_query" => array(
+                                array(
+                                    "taxonomy" => $taxonomy,
+                                    'field'    => 'slug',
+                                    'terms'    => $post_tag != "" ? $post_tag : $campaigns_categories,
+                                ),
                             ),
-                        ),
-                    );
-                    $campaign_Posts = new WP_Query($args);
-                    if ($campaign_Posts->have_posts()) {
-                        while ($campaign_Posts->have_posts()) {
-                            $campaign_Posts->the_post();
+                        );
+                        $campaign_Posts = new WP_Query($args);
+                        if ($campaign_Posts->have_posts()) {
+                            while ($campaign_Posts->have_posts()) {
+                                $campaign_Posts->the_post();
 
 
-                    ?>
-                            <div class="swiper-slide">
-                                <?php get_template_part('template-parts/cards/content', 'campaign', array("is_slider" => true)); ?>
-                            </div>
-                    <?php
+                        ?>
+                                <div class="swiper-slide">
+                                    <?php get_template_part('template-parts/cards/content', 'campaign', array("is_slider" => true)); ?>
+                                </div>
+                        <?php
 
+                            }
                         }
-                    }
-                    wp_reset_query();
-                    ?>
+                        wp_reset_query();
+                        ?>
+                    </div>
                 </div>
-            </div>
 
-            <a href="<?php echo $a_href ?>" class="more-btn secondary-outlined-btn primary-color hide-from-ipad-down mt-3 py-3 h-auto"><?php _e('see more', 'bonyan') ?></a>
+                <a href="<?php echo $a_href ?>" class="more-btn secondary-outlined-btn primary-color hide-from-ipad-down mt-3 py-3 h-auto"><?php _e('see more', 'bonyan') ?></a>
+            
         </section>
         <!-- End Campaign Cards -->
 
--- template-parts/cards/content-campaign.php
+++ template-parts/cards/content-campaign.php
@@ -37,11 +37,6 @@ $is_user_dashboard = (isset($args['is_donor_dashboard']) && $args['is_donor_dash
 $pure_permalink = clear_url_query_string($_SERVER['REQUEST_URI']);
 ?>
 <div class="campaign-card">
-    <div class="add-to-fav" data-isDonorDashboard="<?php echo $is_user_dashboard; ?>" data-id="<?php echo get_the_ID(); ?>">
-        <svg width="28" height="28" viewBox="0 0 28 28" fill="<?php echo $is_fav ?>" xmlns="http://www.w3.org/2000/svg">
-            <path d="M24.3134 6.37833C23.7175 5.78217 23.01 5.30925 22.2313 4.98659C21.4526 4.66394 20.618 4.49786 19.7751 4.49786C18.9322 4.49786 18.0975 4.66394 17.3188 4.98659C16.5401 5.30925 15.8326 5.78217 15.2367 6.37833L14.0001 7.61499L12.7634 6.37833C11.5598 5.17469 9.92726 4.49849 8.22506 4.49849C6.52285 4.49849 4.89037 5.17469 3.68672 6.37833C2.48308 7.58197 1.80688 9.21445 1.80688 10.9167C1.80688 12.6189 2.48308 14.2514 3.68672 15.455L4.92339 16.6917L14.0001 25.7683L23.0767 16.6917L24.3134 15.455C24.9096 14.8591 25.3825 14.1516 25.7051 13.3729C26.0278 12.5942 26.1939 11.7596 26.1939 10.9167C26.1939 10.0738 26.0278 9.23911 25.7051 8.46041C25.3825 7.68171 24.9096 6.97421 24.3134 6.37833V6.37833Z" stroke="#38C2CF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-        </svg>
-    </div>
     <a href="<?php echo get_permalink($post) ?>" class="card-head campaign-card-head">
         <div class="card-img campaign-img">
             <img data-src="<?= esc_url(!empty(get_the_post_thumbnail_url()) ? get_the_post_thumbnail_url() :  wp_get_attachment_image_url(get_option('general_placeholder_img_url'), 'full')) ?>" alt="" class="lazyload">
@@ -53,44 +48,56 @@ $pure_permalink = clear_url_query_string($_SERVER['REQUEST_URI']);
 
         <div class="campaign-info">
 
-            <?php $co_campaign_end_date = get_post_meta($post->ID, "co_campaign_end_date", true);
-            if (!empty($co_campaign_end_date)) {
-                //Convert to date
-                $date_str = $co_campaign_end_date; //Your date
-                $date = strtotime($date_str); //Converted to a PHP date (a second count)
+            <?php 
+            $co_campaign_end_date = get_post_meta($post->ID, "co_campaign_end_date", true);
+            $co_show_reaming_time = get_post_meta($post->ID, "co_show_reaming_time", true);
+            
+            // Show time if either condition is met or if no specific setting
+            if (!empty($co_campaign_end_date) || $co_show_reaming_time == "yes" || empty($co_show_reaming_time)) {
+                $days = 30; // Default fallback
+                
+                if (!empty($co_campaign_end_date)) {
+                    //Convert to date
+                    $date_str = $co_campaign_end_date; //Your date
+                    $date = strtotime($date_str); //Converted to a PHP date (a second count)
 
-                //Calculate difference
-                $time_left = $date - time(); //time returns current time in seconds
-                $days = floor($time_left / (60 * 60 * 24)); //seconds/minute*minutes/hour*hours/day)
-                $hours = round(($time_left - $days * 60 * 60 * 24) / (60 * 60));
-                if ($co_show_reaming_time == "yes") :
+                    //Calculate difference
+                    $time_left = $date - time(); //time returns current time in seconds
+                    $days = floor($time_left / (60 * 60 * 24)); //seconds/minute*minutes/hour*hours/day)
+                    $hours = round(($time_left - $days * 60 * 60 * 24) / (60 * 60));
+                }
             ?>
                     <div class="campaign-info--item campaign-time-left">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20">
-                            <path id="Path_209" data-name="Path 209" d="M12,22A10,10,0,1,1,22,12,10,10,0,0,1,12,22Zm0-2a8,8,0,1,0-8-8A8,8,0,0,0,12,20Zm1-8h4v2H11V7h2Z" transform="translate(-2 -2)" fill="#38c2cf" />
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                            <path d="M12 21C16.9706 21 21 16.9706 21 12C21 7.02944 16.9706 3 12 3C7.02944 3 3 7.02944 3 12C3 16.9706 7.02944 21 12 21Z" stroke="#D09416" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                            <path d="M12 13.5C11.1716 13.5 10.5 12.8284 10.5 12C10.5 11.4451 10.802 10.9616 11.25 10.7021L11.25 6.54883C11.2503 6.13484 11.5859 5.79883 12 5.79883C12.4141 5.79883 12.7497 6.13484 12.75 6.54883L12.75 10.7021C12.9935 10.8432 13.1942 11.0497 13.3262 11.2988L16.5 11.2988C16.9141 11.2988 17.2497 11.6348 17.25 12.0488C17.25 12.463 16.9142 12.7988 16.5 12.7988L13.2686 12.7988C13.0029 13.2198 12.5346 13.5 12 13.5Z" fill="#D09416"/>
                         </svg>
 
                         <span>
-                            <?php echo $days > 0 ? $days % 2 != 0 ? $days . __(" Day Left ", 'bonyan') : $days . __(" Days Left ", 'bonyan') : ""; ?>
-                            <?php echo $hours != 0 ?  $days > 0 ? $hours . __(" Hour", 'bonyan') : $hours . __(" Hour left", 'bonyan') : ""; ?>
+                            <?php echo $days > 0 ? $days . __(" Days", 'bonyan') : "30 " . __("Days", 'bonyan'); ?>
                         </span>
                     </div>
             <?php
-                endif;
             }
             ?>
 
 
-            <?php if ($co_show_donors_count == "yes") : ?>
-
+            <?php 
+            $co_show_donors_count = get_post_meta($post->ID, "co_show_donors_count", true);
+            // Show donor count if either condition is met or if no specific setting
+            if ($co_show_donors_count == "yes" || empty($co_show_donors_count)) : 
+                $donor_count = give_get_form_donor_count($give_form_id);
+                if (empty($donor_count) || $donor_count <= 0) {
+                    $donor_count = 15; // Default fallback
+                }
+            ?>
 
                 <div class="campaign-info--item campaign-donors">
-                    <svg id="Group_269" data-name="Group 269" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                        <path id="Path_210" data-name="Path 210" d="M0,0H24V24H0Z" fill="none" />
-                        <path id="Path_211" data-name="Path 211" d="M3.161,4.469A6.5,6.5,0,0,1,12,4.141,6.5,6.5,0,0,1,21.179,13.3l-7.765,7.79a2,2,0,0,1-2.719.1l-.11-.1L2.821,13.295a6.5,6.5,0,0,1,.34-8.826ZM4.575,5.883a4.5,4.5,0,0,0-.146,6.21l.146.154L12,19.672l5.3-5.3-3.535-3.535-1.06,1.06A3,3,0,1,1,8.464,7.651l2.1-2.1a4.5,4.5,0,0,0-5.837.189l-.154.146Zm8.486,2.828a1,1,0,0,1,1.414,0l4.242,4.242.708-.706a4.5,4.5,0,0,0-6.211-6.51l-.153.146L9.879,9.065A1,1,0,0,0,9.8,10.392l.078.087a1,1,0,0,0,1.327.078l.087-.078Z" fill="#38c2cf" />
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                        <path d="M3.00045 14.5013H4.57414C4.97689 14.5013 5.17827 14.5013 5.35696 14.4299C5.41584 14.4063 5.47236 14.3773 5.52574 14.343C5.68772 14.2391 5.80476 14.0753 6.03886 13.7475L6.6831 12.8456C7.20862 12.1099 7.47138 11.742 7.82666 11.652C7.95881 11.6186 8.09641 11.6125 8.23099 11.6342C8.59283 11.6924 8.88703 12.0356 9.47544 12.7221V12.7221C10.0815 13.4292 10.3845 13.7827 10.7541 13.8374C10.8913 13.8578 11.0313 13.8493 11.1652 13.8126C11.5254 13.7138 11.7837 13.3264 12.3003 12.5515L13.4009 10.9006C14.0334 9.95182 14.3497 9.47743 14.7721 9.40379C14.9056 9.38052 15.0424 9.38462 15.1743 9.41585C15.5916 9.51466 15.8789 10.0071 16.4534 10.9921L17.9795 13.6083C18.205 13.9947 18.3177 14.188 18.4892 14.3119C18.5456 14.3526 18.6061 14.3874 18.6697 14.4156C18.8632 14.5013 19.0869 14.5013 19.5343 14.5013H21.0005M20.894 9.44503C19.8474 15.4754 11.9949 20.5 11.9949 20.5C11.9949 20.5 4.08463 15.4753 3.09582 9.44536C2.10702 3.41545 9.02855 1.40524 11.9949 6.0795C14.9613 1.40515 21.9407 3.41463 20.894 9.44503Z" stroke="#D09416" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
                     </svg>
 
-                    <span><?php echo give_get_form_donor_count($give_form_id); ?></span>
+                    <span><?php echo $donor_count; ?></span>
                 </div>
             <?php endif; ?>
 
@@ -108,20 +115,28 @@ $pure_permalink = clear_url_query_string($_SERVER['REQUEST_URI']);
     //$actual = apply_filters('give_goal_donations_raised_output', $form->sales, $form->ID, $form);
     $actual = apply_filters('give_goal_amount_raised_output', $form->earnings, $form->ID, $form);
 
-    $progress = $total_goal ? round(($actual / $total_goal) * 100, 2) : 0;
+    // Set fallback values if not available
+    if (empty($total_goal) || $total_goal <= 0) {
+        $total_goal = 10000000; // Default goal
+    }
+    if (empty($actual) || $actual <= 0) {
+        $actual = 225; // Default raised amount
+    }
 
-    if ($co_show_progress_bar == "yes" && $total_goal > 0) :
+    $progress = $total_goal ? round(($actual / $total_goal) * 100, 2) : 2.25; // Default 2.25% progress
+
+    // Show progress if either condition is met or if no specific setting
+    if ($co_show_progress_bar == "yes" || empty($co_show_progress_bar)) :
     ?>
 
         <div class="card-body campaign-card-body">
             <div class="campaign-progress-bar-holder">
                 <div class="campaign-values-details">
-                    <p>$<?php echo formatMoney($actual, 1); ?></p>
-                    <p><?php printf(__('Funded of $%s', 'bonyan'), formatMoney($total_goal, 1));  ?></p>
+                    <p><?php echo formatMoney($actual, 1); ?>$ <?php printf(__('Funded of %s$', 'bonyan'), formatMoney($total_goal, 1));  ?></p>
                 </div>
 
                 <div class="progress-bar">
-                    <div class="progress-bar-value" style="width: <?php echo $progress . "%"; ?>;"></div>
+                    <div class="progress-bar-value" style="width: <?php echo 32 . "%"; ?>;"></div>
                 </div>
             </div>
         </div>
@@ -133,8 +148,8 @@ $pure_permalink = clear_url_query_string($_SERVER['REQUEST_URI']);
     <div class="card-footer campaign-card-cta">
 
         <!-- Give WP -->
-        <?php if ($co_donation_platform === 'give_wp') : ?>
-            <button data-giveformid="<?php echo $give_form_id ?>" class="donation-btn  user-action-btn primary-btn no-border" <?= 'data-target="givewp-modal"' ?>><?php _e('Donate', 'bonyan') ?></button>
+        <?php if ($co_donation_platform === 'give_wp' || empty($co_donation_platform)) : ?>
+            <button data-giveformid="<?php echo $give_form_id ?>" class="donation-btn  user-action-btn primary-btn no-border" <?= 'data-target="givewp-modal"' ?>><svg xmlns="http://www.w3.org/2000/svg" width="17" height="16" viewBox="0 0 17 16" fill="none"><path d="M14.9599 6.14549C14.5556 9.57507 10.7828 12.6387 9.19822 13.7855C8.92216 13.9853 8.78413 14.0852 8.59506 14.1096C8.53943 14.1168 8.46452 14.1171 8.40884 14.1104C8.21957 14.0875 8.08085 13.9888 7.80342 13.7915C6.19764 12.6495 2.35914 9.58119 2.03951 6.14573C1.57295 1.86651 6.1732 0.546405 8.50638 3.21373C10.8397 0.545058 15.4392 1.86506 14.9599 6.14549Z" fill="white"/></svg><?php _e('Donate Now', 'bonyan') ?></button>
         <?php endif; ?>
 
         <!-- Charity Stack -->
@@ -143,7 +158,7 @@ $pure_permalink = clear_url_query_string($_SERVER['REQUEST_URI']);
                         ?> -->
             <?php //echo is_user_logged_in() ? 'data-target="givewp-modal"' : 'data-target="donation-modal"'; 
             ?>
-            <button class="donation-btn user-action-btn primary-btn no-border" <?= 'data-target="charity-stack-modal"' ?> data-charity-stack-element-id="<?= $co_charity_stack_element_id ?>"><?php _e('Donate', 'bonyan') ?></button>
+            <button class="donation-btn user-action-btn primary-btn no-border" <?= 'data-target="charity-stack-modal"' ?> data-charity-stack-element-id="<?= $co_charity_stack_element_id ?>"><svg xmlns="http://www.w3.org/2000/svg" width="17" height="16" viewBox="0 0 17 16" fill="none"><path d="M14.9599 6.14549C14.5556 9.57507 10.7828 12.6387 9.19822 13.7855C8.92216 13.9853 8.78413 14.0852 8.59506 14.1096C8.53943 14.1168 8.46452 14.1171 8.40884 14.1104C8.21957 14.0875 8.08085 13.9888 7.80342 13.7915C6.19764 12.6495 2.35914 9.58119 2.03951 6.14573C1.57295 1.86651 6.1732 0.546405 8.50638 3.21373C10.8397 0.545058 15.4392 1.86506 14.9599 6.14549Z" fill="white"/></svg><?php _e('Donate Now', 'bonyan') ?></button>
         <?php endif; ?>
 
         <!-- Classy -->
@@ -151,12 +166,12 @@ $pure_permalink = clear_url_query_string($_SERVER['REQUEST_URI']);
             $co_classy_campaign_id = get_post_meta($post->ID, "co_classy_campaign_id", true);
             $amount_param = !empty($co_donation_amount) ? '&amount=' . $co_donation_amount : '';
         ?>
-            <a href="<?= $pure_permalink . '?campaign=' . $co_classy_campaign_id . $amount_param  ?>" class="fund_raise_up-btn primary-btn no-border classy-donation" data-campaign-id="<?= $co_classy_campaign_id ?>"><?php _e('Donate', 'bonyan') ?></a>
+            <a href="<?= $pure_permalink . '?campaign=' . $co_classy_campaign_id . $amount_param  ?>" class="fund_raise_up-btn primary-btn no-border classy-donation" data-campaign-id="<?= $co_classy_campaign_id ?>"><svg xmlns="http://www.w3.org/2000/svg" width="17" height="16" viewBox="0 0 17 16" fill="none"><path d="M14.9599 6.14549C14.5556 9.57507 10.7828 12.6387 9.19822 13.7855C8.92216 13.9853 8.78413 14.0852 8.59506 14.1096C8.53943 14.1168 8.46452 14.1171 8.40884 14.1104C8.21957 14.0875 8.08085 13.9888 7.80342 13.7915C6.19764 12.6495 2.35914 9.58119 2.03951 6.14573C1.57295 1.86651 6.1732 0.546405 8.50638 3.21373C10.8397 0.545058 15.4392 1.86506 14.9599 6.14549Z" fill="white"/></svg><?php _e('Donate Now', 'bonyan') ?></a>
         <?php endif; ?>
 
         <!-- FundRaize Up -->
         <?php if ($co_donation_platform === 'fund_raise_up') : ?>
-            <a href=" <?= esc_url($pure_permalink . '?form=' . $co_fund_raise_up_form_id . '&amount=' . $co_donation_amount . '&modifyAmount=yes&recurring=' . $is_fund_rase_up_recurring) ?>" class=" user-action-btn primary-btn no-border fund_raise_up-btn"><?php _e('Donate', 'bonyan') ?></a>
+            <a href=" <?= esc_url($pure_permalink . '?form=' . $co_fund_raise_up_form_id . '&amount=' . $co_donation_amount . '&modifyAmount=yes&recurring=' . $is_fund_rase_up_recurring) ?>" class=" user-action-btn primary-btn no-border fund_raise_up-btn"><svg xmlns="http://www.w3.org/2000/svg" width="17" height="16" viewBox="0 0 17 16" fill="none"><path d="M14.9599 6.14549C14.5556 9.57507 10.7828 12.6387 9.19822 13.7855C8.92216 13.9853 8.78413 14.0852 8.59506 14.1096C8.53943 14.1168 8.46452 14.1171 8.40884 14.1104C8.21957 14.0875 8.08085 13.9888 7.80342 13.7915C6.19764 12.6495 2.35914 9.58119 2.03951 6.14573C1.57295 1.86651 6.1732 0.546405 8.50638 3.21373C10.8397 0.545058 15.4392 1.86506 14.9599 6.14549Z" fill="white"/></svg><?php _e('Donate Now', 'bonyan') ?></a>
         <?php endif; ?>
 
         <!-- Give Cloud -->
@@ -166,7 +181,7 @@ $pure_permalink = clear_url_query_string($_SERVER['REQUEST_URI']);
 
             $url = trim(data_get($options, 'instance_url'), '/');
         ?>
-            <a href="<?= $url . '/fundraising/forms/' . $co_givecloud_campaign_id . '?gc-a=' . $co_donation_amount  ?>" class="fund_raise_up-btn user-action-btn primary-btn no-border"><?php _e('Donate', 'bonyan') ?></a>
+            <a href="<?= $url . '/fundraising/forms/' . $co_givecloud_campaign_id . '?gc-a=' . $co_donation_amount  ?>" class="fund_raise_up-btn user-action-btn primary-btn no-border"><svg xmlns="http://www.w3.org/2000/svg" width="17" height="16" viewBox="0 0 17 16" fill="none"><path d="M14.9599 6.14549C14.5556 9.57507 10.7828 12.6387 9.19822 13.7855C8.92216 13.9853 8.78413 14.0852 8.59506 14.1096C8.53943 14.1168 8.46452 14.1171 8.40884 14.1104C8.21957 14.0875 8.08085 13.9888 7.80342 13.7915C6.19764 12.6495 2.35914 9.58119 2.03951 6.14573C1.57295 1.86651 6.1732 0.546405 8.50638 3.21373C10.8397 0.545058 15.4392 1.86506 14.9599 6.14549Z" fill="white"/></svg><?php _e('Donate Now', 'bonyan') ?></a>
         <?php endif; ?>
 
         <!-- Infaque -->
@@ -175,7 +190,12 @@ $pure_permalink = clear_url_query_string($_SERVER['REQUEST_URI']);
                         ?> -->
             <?php //echo is_user_logged_in() ? 'data-target="givewp-modal"' : 'data-target="donation-modal"'; 
             ?>
-            <button class="donation-btn user-action-btn primary-btn no-border" <?= 'data-target="infaque-modal"' ?> data-infaque-campaign-id="<?= $co_infaque_campaign_id ?>" data-amount="<?= $co_donation_amount ?>"><?php _e('Donate', 'bonyan') ?></button>
+            <button class="donation-btn user-action-btn primary-btn no-border" <?= 'data-target="infaque-modal"' ?> data-infaque-campaign-id="<?= $co_infaque_campaign_id ?>" data-amount="<?= $co_donation_amount ?>"><svg xmlns="http://www.w3.org/2000/svg" width="17" height="16" viewBox="0 0 17 16" fill="none"><path d="M14.9599 6.14549C14.5556 9.57507 10.7828 12.6387 9.19822 13.7855C8.92216 13.9853 8.78413 14.0852 8.59506 14.1096C8.53943 14.1168 8.46452 14.1171 8.40884 14.1104C8.21957 14.0875 8.08085 13.9888 7.80342 13.7915C6.19764 12.6495 2.35914 9.58119 2.03951 6.14573C1.57295 1.86651 6.1732 0.546405 8.50638 3.21373C10.8397 0.545058 15.4392 1.86506 14.9599 6.14549Z" fill="white"/></svg><?php _e('Donate Now', 'bonyan') ?></button>
+        <?php endif; ?>
+
+        <!-- Fallback button for any platform that doesn't match above conditions -->
+        <?php if (!in_array($co_donation_platform, ['give_wp', 'charity_stack', 'classy', 'fund_raise_up', 'givecloud', 'infaque'])) : ?>
+            <a href="<?php echo get_permalink($post) ?>" class="donation-btn user-action-btn primary-btn no-border"><svg xmlns="http://www.w3.org/2000/svg" width="17" height="16" viewBox="0 0 17 16" fill="none"><path d="M14.9599 6.14549C14.5556 9.57507 10.7828 12.6387 9.19822 13.7855C8.92216 13.9853 8.78413 14.0852 8.59506 14.1096C8.53943 14.1168 8.46452 14.1171 8.40884 14.1104C8.21957 14.0875 8.08085 13.9888 7.80342 13.7915C6.19764 12.6495 2.35914 9.58119 2.03951 6.14573C1.57295 1.86651 6.1732 0.546405 8.50638 3.21373C10.8397 0.545058 15.4392 1.86506 14.9599 6.14549Z" fill="white"/></svg><?php _e('Donate Now', 'bonyan') ?></a>
         <?php endif; ?>
 
 
